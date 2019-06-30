0df74bd4fc8ec7ebe1347f9443c0c22549b03dcd
Tiago Hucs <tiagohucs@gmail.com>
2019-06-30 12:52:26 -0300

    ajuste controller

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -2,10 +2,8 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
 @SpringBootApplication
 public class Main {
 
