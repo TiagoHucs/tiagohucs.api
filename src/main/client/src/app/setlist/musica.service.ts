dba05ca231e321f546b0a6e83d06ef6a910b12fd
Tiago Hucs <tiagohucs@gmail.com>
2019-06-30 12:30:02 -0300

    add controller to main

--- src/main/client/src/app/setlist/musica.service.ts
+++ src/main/client/src/app/setlist/musica.service.ts
@@ -8,15 +8,15 @@ import { HttpClient } from '@angular/common/http';
 export class MusicaService {
 
   URL_API_LOCAL: String = 'http://localhost:8080/';
-  URL_API_PROD: String = ''; 
 
   constructor(private http: HttpClient) { }
 
   public listar(): Observable<any> {
-    return this.http.get<any[]>(`${this.URL_API_PROD}musicas/list`);
+    // return this.http.get<any[]>(`${this.URL_API_PROD}musicas/list`);
+    return this.http.get<any[]>(`musicas/list`);
   }
 
   public salvar(musica: any): Observable<any> {
-    return this.http.post(`${this.URL_API_PROD}musicas/save`,musica);
+    return this.http.post(`musicas/save`,musica);
   }
 }
--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -2,8 +2,10 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 @SpringBootApplication
 public class Main {
 
