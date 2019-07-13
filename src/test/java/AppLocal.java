e6a4df86942a9cee0343cef6ef54fa2301991126
Tiago Hucs <tiagohucs@gmail.com>
2019-07-13 12:56:10 -0300

    profiles in application.properties

--- src/test/java/AppLocal.java
+++ src/test/java/AppLocal.java
@@ -1,12 +1,8 @@
 import com.example.Main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication()
-@Configuration
-@Profile("release")
 public class AppLocal {
 
     public static void main(String[] args) throws Exception {
