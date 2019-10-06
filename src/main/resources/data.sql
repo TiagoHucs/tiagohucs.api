8da6d425d1b1004417244dfd78ee7c311a7d29ae
TiagoHucs <tiagohucs@gmail.com>
2019-10-06 18:27:48 -0300

    data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO cliente (id, nome, cpfcnpj, tipo_cliente) VALUES (1, 'Patricia LTDA', '0123456789' , 0);
+INSERT INTO produto (id, nome, valor, tipo_medida) VALUES (1, 'Tijolo', 0.5 , 0),(2, 'Cimento', 5 , 1);
