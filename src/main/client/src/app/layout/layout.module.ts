1b557d1fad8a5318ca0f2fff277746ac96b747e7
TiagoHucs <tiagohucs@gmail.com>
2020-01-15 23:05:53 -0300

    bugfix ngx-currency

--- src/main/client/src/app/layout/layout.module.ts
+++ src/main/client/src/app/layout/layout.module.ts
@@ -6,7 +6,6 @@ import { HeaderComponent } from './components/header/header.component';
 import { LayoutRoutingModule } from './layout-routing.module';
 import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { NgxCurrencyModule } from 'ngx-currency';
 import { OrcamentoEditarComponent } from './orcamentos/orcamento-editar/orcamento-editar.component';
 import { OrcamentoListarComponent } from './orcamentos/orcamento-listar/orcamento-listar.component';
 import { ClientesModule } from './clientes/clientes.module';
@@ -29,7 +28,6 @@ import { PublicacaoModule } from './publicacao/publicacao.module';
     LayoutRoutingModule,
     FormsModule,
     ReactiveFormsModule,
-    NgxCurrencyModule,
     ClientesModule,
     SnotifyModule
   ],
