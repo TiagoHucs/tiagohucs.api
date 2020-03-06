5b007b23d03ca634882e5283878bb3b8d06cf19a
TiagoHucs <tiagohucs@gmail.com>
2020-03-06 09:17:42 -0300

    ajustes

--- src/main/java/com/hucs/negocio/perfil/PerfilRepository.java
+++ src/main/java/com/hucs/negocio/perfil/PerfilRepository.java
@@ -1,6 +1,5 @@
 package com.hucs.negocio.perfil;
 
-import com.hucs.negocio.cliente.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
